99fb42b71315315173801c928957085eab0010bc
Topi Mansikkaviita <tomansi@gmail.com>
2025-02-05 00:54:36 +0200

    Perusversio

--- laskukone/laskukone.go
+++ laskukone/laskukone.go
@@ -6,27 +6,35 @@ import (
 	"strconv"
 )
 
+func readNumber(msg string) float64 {
+	var number float64
+	fmt.Print(msg)
+	fmt.Scan(&number)
+	return number
+}
+
+func readOperation(msg string) string {
+	var operation string
+	fmt.Print(msg)
+	fmt.Scan(&operation)
+	return operation
+}
+
 func main() {
 	// Laskettavat luvut
 	var luku1, luku2 float64
-
 	// Laskutoimitus
-	var lt string
+	var operaatio string
 
 	// Kysy ensimmäinen luku.
-	fmt.Println("Anna ensimmäinen luku:")
-	fmt.Scanf("%f", &luku1)
-
-	// Kysy toinen luku.
-	fmt.Println("Anna toinen luku:")
-	fmt.Scanf("%f", &luku2)
+	luku1 = readNumber("Anna ensimmäinen luku: ")
+	luku2 = readNumber("Anna toinen luku: ")
 
 	// Kysy laskutoimitus.
-	fmt.Println("Anna laskutoimitus:")
-	fmt.Scanf("%s", &lt)
+	operaatio = readOperation("Anna laskutoimitus: ")
 
 	// Tulostetaan, mitä luettiin.
-	fmt.Println("Lasken: ", luku1, lt, luku2)
+	fmt.Println("Lasken: ", luku1, operaatio, luku2)
 
 	// Tulos sisältää laskutoimituksen tuloksen.
 	var tulos float64
@@ -35,23 +43,33 @@ func main() {
 	var virhe string
 
 	// Suoritetaan laskutoimituksen mukainen laskutoimitus.
-	switch lt {
+	switch operaatio {
 	case "+":
 		tulos = luku1 + luku2
 		virhe = ""
+	case "-":
+		tulos = luku1 - luku2
+		virhe = ""
+
+	case "*":
+		tulos = luku1 * luku2
+		virhe = ""
+
+	case "/":
+		tulos = luku1 / luku2
+		virhe = ""
+
 	default:
 		tulos = luku1 + rand.Float64()*luku2
-		virhe = "En tunnistanut laskutoimitusta '"+lt+"'. "+
-			"Tässä sinulle satunnaisluku: "+
+		virhe = "En tunnistanut laskutoimitusta '" + operaatio + "'. " +
+			"Tässä sinulle satunnaisluku: " +
 			strconv.FormatFloat(tulos, 'G', 3, 64)
 	}
 
 	// Tulostetaan laskutoimituksen tulos tai virhe.
-	if virhe=="" {
+	if virhe == "" {
 		fmt.Println("Tulos on", tulos)
 	} else {
 		fmt.Println("Virhe:", virhe)
 	}
 }
-
- 
