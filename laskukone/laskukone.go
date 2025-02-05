f4c669eb30299952bb4f37f5ea65670bef8b4461
Topi Mansikkaviita <tomansi@gmail.com>
2025-02-05 19:37:34 +0200

    Jakoa aliohjelmiin

--- laskukone/laskukone.go
+++ laskukone/laskukone.go
@@ -26,29 +26,12 @@ func readOperation(msg string) string {
 	return operation
 }
 
-func main() {
-	// Laskettavat luvut
-	var luku1, luku2 float64
-	// Laskutoimitus
-	var operaatio string
-
-	// Kysy ensimmäinen luku.
-	luku1 = readNumber("Anna ensimmäinen luku: ")
-	luku2 = readNumber("Anna toinen luku: ")
-
-	// Kysy laskutoimitus.
-	operaatio = readOperation("Anna laskutoimitus: ")
-
-	// Tulostetaan, mitä luettiin.
-	fmt.Println("Lasken: ", luku1, operaatio, luku2)
-
-	// Tulos sisältää laskutoimituksen tuloksen.
+func laske(luku1, luku2 float64, operaatio string) {
 	var tulos float64
-
-	// Virhe sisältää kuvauksen virheestä, mutta on tyhjä, jos virhettä ei tapahtunut.
 	var virhe string
+	// Lasketaan laskutoimitus.
 
-	// Suoritetaan laskutoimituksen mukainen laskutoimitus.
+	fmt.Println("Lasken: ", luku1, operaatio, luku2)
 	virhe = ""
 	switch operaatio {
 	case "+":
@@ -59,21 +42,29 @@ func main() {
 		tulos = luku1 * luku2
 	case "/":
 		if luku2 == 0 {
-			virhe = "Nollalla ei voi jakaa."
-			break
+			fmt.Println("Nollalla ei voi jakaa.")
+		} else {
+			tulos = luku1 / luku2
 		}
-		tulos = luku1 / luku2
 	default:
 		tulos = luku1 + rand.Float64()*luku2
 		virhe = "En tunnistanut laskutoimitusta '" + operaatio + "'. " +
 			"Tässä sinulle satunnaisluku: " +
 			strconv.FormatFloat(tulos, 'G', 3, 64)
 	}
-
 	// Tulostetaan laskutoimituksen tulos tai virhe.
 	if virhe == "" {
 		fmt.Println("Tulos on", tulos)
 	} else {
 		fmt.Println("Virhe:", virhe)
 	}
+}
+
+func main() {
+	// Kysy ensimmäinen luku.
+	luku1 := readNumber("Anna ensimmäinen luku: ")
+	luku2 := readNumber("Anna toinen luku: ")
+	// Kysy laskutoimitus.
+	operaatio := readOperation("Anna laskutoimitus: ")
+	laske(luku1, luku2, operaatio)
 }
