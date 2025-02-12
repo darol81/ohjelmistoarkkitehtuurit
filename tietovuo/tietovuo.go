da04fd8b6a8bb778c735863ab7a3ec3ce4172e75
Topi Mansikkaviita <tomansi@gmail.com>
2025-02-12 21:50:30 +0200

    Tietovuo, laskukone

--- tietovuo/tietovuo.go
+++ tietovuo/tietovuo.go
@@ -12,7 +12,6 @@ import (
 
 func evaluate(expression string) (interface{}, error) {
 	env := map[string]interface{}{}
-
 	program, err := expr.Compile(expression, expr.Env(env))
 	if err != nil {
 		return nil, err
@@ -24,12 +23,19 @@ func evaluate(expression string) (interface{}, error) {
 	return output, nil
 }
 
+func calculate(luku1, luku2 float64, op string) {
+	expression := fmt.Sprintf("%f %s %f", luku1, op, luku2)
+	result, _ := evaluate(expression)
+	fmt.Println("Lasken:", luku1, op, luku2)
+	fmt.Println("Tulos on", result)
+}
+
 func readInput[T int | float64 | string](msg string) T {
 	fmt.Print(msg)
 	reader := bufio.NewReader(os.Stdin)
 	input, _ := reader.ReadString('\n')
-	input = strings.TrimSuffix(input, "\n")
-	input = strings.TrimSuffix(input, "\r")
+	input = strings.TrimSpace(input)
+
 	var result interface{}
 	var t T
 	switch any(t).(type) {
@@ -43,16 +49,25 @@ func readInput[T int | float64 | string](msg string) T {
 	return result.(T)
 }
 
-func calculate(luku1, luku2 float64, op string) {
-	expression := fmt.Sprintf("%f %s %f", luku1, op, luku2)
-	result, _ := evaluate(expression)
-	fmt.Println("Lasken: ", luku1, op, luku2)
-	fmt.Println("Tulos on", result)
+func sendValue[T int | float64 | string](value T, channel chan T) {
+	channel <- value
 }
 
 func main() {
-	luku1 := readInput[float64]("Anna ensimmäinen luku: ")
-	luku2 := readInput[float64]("Anna toinen luku: ")
-	operaatio := readInput[string]("Anna laskutoimitus: ")
+
+	chan1 := make(chan float64)
+	chan2 := make(chan float64)
+	chan_op := make(chan string)
+
+	value1 := readInput[float64]("Anna ensimmäinen luku: ")
+	value2 := readInput[float64]("Anna toinen luku: ")
+	value3 := readInput[string]("Anna laskutoimitus: ")
+	go sendValue(value1, chan1)
+	go sendValue(value2, chan2)
+	go sendValue(value3, chan_op)
+
+	luku1 := <-chan1
+	luku2 := <-chan2
+	operaatio := <-chan_op
 	calculate(luku1, luku2, operaatio)
 }
